7a1a010e2a31641af0c9e1b0611da221d7508f32
GasparScienza <gasparscienza@hotmail.com>
2024-04-04 00:33:44 -0300

    primer commit

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crear Producto</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container"><br>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Crear producto
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('products.add') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="">Descripcion</label>
+                                <input type="text" class="form-control" name="description">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Precio</label>
+                                <input type="number" class="form-control" name="price">
+                            </div><br>
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                            <a href="{{ route('products.index') }}" class="btn btn-danger">Cancelar</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\Product;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::get('products',function(){
+    $products = Product::orderBy('description', 'asc')->get();
+    return view('products.index', compact('products'));//Retornamos una vista 
+}) -> name('products.index');
+
+Route::get('products/create', function(){
+    return view('products.create');
+})-> name('products.create');
+
+Route::Post('/products', function(Request $request){
+    $product = new Product();
+    $product->description = $request->input('description');
+    $product->price = $request->input('price');
+    $product->save();
+    return redirect()->route('products.index')->with('info', 'Producto creado');
+})->name('products.add');
+
+Route::delete('products/{id}', function($id){
+    $product = Product::findOrFail($id);
+    $product->delete();
+    return redirect()->route('products.index')->with('info','Producto eliminado');
+}) ->name('products.destroy');
